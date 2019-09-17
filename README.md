1ca4e800dbf495edc7d4cec56281316d52487805
mitchellhamilton <mitchell@hamil.town>
2019-09-17 14:03:05 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Changesets Release Action
+
+This action for [Changesets](https://github.com/atlassian/changesets) comments on PRs with whether a PR has a changeset or not and links to documentation explaining to contributors how to create a changeset.
+
+## Usage
+
+Create a file at `.github/workflows/changeset-check.yml` with the following content.
+
+```yml
+name: Changeset Check
+
+on: pull_request
+
+jobs:
+  check:
+    name: Changeset Check
+    runs-on: ubuntu-latest
+    steps:
+      - name: Comment on PR
+        uses: changesets/check-action@master
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
---
+++ index.ts
@@ -0,0 +1,87 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { IssuesListCommentsParams, PullsListFilesParams } from "@octokit/rest";
+
+const changesetActionSignature = `<!-- changeset-check-action-signature -->`;
+
+function getAbsentMessage(commitSha: string) {
+  return `###  💥  No Changeset
+Latest commit: ${commitSha}
+Merging this PR will not cause any packages to be released. If these changes should not cause updates to packages in this repo, this is fine 🙂
+**If these changes should be published to npm, you need to add a changeset.**
+[Click here to learn what changesets are, and how to add one](https://github.com/Noviny/changesets/blob/master/docs/adding-a-changeset.md).
+${changesetActionSignature}`;
+}
+function getApproveMessage(commitSha: string) {
+  return `###  🦋  Changeset is good to go
+Latest commit: ${commitSha}
+**We got this.**
+Not sure what this means? [Click here to learn what changesets are](https://github.com/Noviny/changesets/blob/master/docs/adding-a-changeset.md).
+${changesetActionSignature}`;
+}
+
+const getCommentId = (
+  octokit: github.GitHub,
+  params: IssuesListCommentsParams
+) =>
+  octokit.issues.listComments(params).then(comments => {
+    const changesetBotComment = comments.data.find(comment =>
+      comment.body.includes(changesetActionSignature)
+    );
+    return changesetBotComment ? changesetBotComment.id : null;
+  });
+
+const getHasChangeset = (
+  octokit: github.GitHub,
+  params: PullsListFilesParams
+) =>
+  octokit.pulls.listFiles(params).then(files => {
+    const changesetFiles = files.data.filter(
+      file => file.filename.startsWith(".changeset") && file.status === "added"
+    );
+    return changesetFiles.length > 0;
+  });
+
+(async () => {
+  let githubToken = process.env.GITHUB_TOKEN;
+
+  if (!githubToken) {
+    core.setFailed("Please add the GITHUB_TOKEN to the changesets action");
+    return;
+  }
+  let repo = `${github.context.repo.owner}/${github.context.repo.repo}`;
+
+  const octokit = new github.GitHub(githubToken);
+  console.log(JSON.stringify(github.context.payload, null, 2));
+  const [commentId, hasChangeset] = await Promise.all([
+    getCommentId(octokit, {
+      issue_number: github.context.payload.pull_request!.number,
+      ...github.context.repo
+    }),
+    getHasChangeset(octokit, {
+      pull_number: github.context.payload.pull_request!.number,
+      ...github.context.repo
+    })
+  ]);
+  let latestCommit = github.context.sha;
+
+  let message = hasChangeset
+    ? getApproveMessage(github.context.sha)
+    : getAbsentMessage(github.context.sha);
+
+  if (commentId) {
+    return octokit.issues.updateComment({
+      comment_id: commentId,
+      body: message,
+      ...github.context.repo
+    });
+  }
+  return octokit.issues.createComment({
+    ...github.context.repo,
+    issue_number: github.context.payload.pull_request!.number,
+    body: message
+  });
+})().catch(err => {
+  console.error(err);
+  core.setFailed(err.message);
+});
